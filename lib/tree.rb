b396a32f406c2ce913a21eb9e0a58fba105b6d22
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2022-01-16 14:07:35 -0600

    Completed assignment

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,4 +2,15 @@ class Tree
   def initialize(root = nil)
     @root = root
   end
+
+  def get_element_by_id(id)
+    tree = [@root]
+    while tree.count > 0
+      node = tree[0]
+      return node if node[:id] == id
+      tree += node[:children]
+      tree.shift
+    end
+    nil
+  end
 end
